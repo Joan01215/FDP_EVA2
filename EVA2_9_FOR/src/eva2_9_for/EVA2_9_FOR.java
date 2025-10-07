0e9aaf5804eb3e2466e1255ffff48ac305bf7553
Joan01215 <joangalaxy07@gmail.com>
2025-10-07 01:18:25 -0600

    Add files via upload

---
+++ EVA2_9_FOR/src/eva2_9_for/EVA2_9_FOR.java
@@ -0,0 +1,18 @@
+package eva2_9_for;
+
+public class EVA2_9_FOR {
+
+    public static void main(String[] args) {
+                //IMPRIMIR LOS NUMEROS DEL 1 A 20
+        for(int i = 1; i <= 20;  i++)
+            System.out.println( i + " -");
+        //IMPRIMIR LOS NUMEROS PARES DEL 0 AL 100
+        for(int i = 0; i <= 100; i+=2)
+            System.out.println( i+ " - ");
+        for(int i = 100; i <= 100; i-=2)
+            System.out.println( i+ " - ");
+        //IMRIMIR LA LISTA DE NUMEROS ENTRE DOS VALORES QUE
+        //EL USUAIO ESCOGA
+    }
+    
+}
