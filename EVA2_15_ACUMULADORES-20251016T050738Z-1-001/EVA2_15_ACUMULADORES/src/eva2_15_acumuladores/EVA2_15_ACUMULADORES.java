a6eb35e0d977a3161eb08624171bee27ea99a647
Joan01215 <joangalaxy07@gmail.com>
2025-10-15 23:14:32 -0600

    Add files via upload

---
+++ EVA2_15_ACUMULADORES-20251016T050738Z-1-001/EVA2_15_ACUMULADORES/src/eva2_15_acumuladores/EVA2_15_ACUMULADORES.java
@@ -0,0 +1,19 @@
+package eva2_15_acumuladores;
+
+public class EVA2_15_ACUMULADORES {
+
+    public static void main(String[] args) {
+        int cuenta = 0;
+        //ASIGNACION
+        System.out.println(cuenta);
+        cuenta = cuenta+ 100;//ACUMULADOR
+        System.out.println(cuenta);
+        cuenta = cuenta + 1000;
+        System.out.println(cuenta);
+        cuenta = cuenta - 800;//CEUNTA -= 800;
+        cuenta += 2000;
+        System.out.println(cuenta);//CUENTA = CUENTA +2000
+        
+    }
+    
+}
